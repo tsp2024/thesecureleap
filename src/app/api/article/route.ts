f4659f3064bc5f40903f4e70bd7a440e50cd82e4
Random <tabishnizami456@gmail.com>
2025-03-08 17:10:23 +0530

    changes

--- src/app/api/article/route.ts
+++ src/app/api/article/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import db from "@/db/db";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
   try {
     const articles = await db.article.findMany();
